436b42fecb3a3ad9ed446250596ff4a918825fab
Tracey Treat <tracey_treat@yahoo.com>
2022-10-16 11:47:11 -0500

    Stretch goals

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -75,24 +75,50 @@ console.log('Finding value 1 in array of 4, 5, 6:', find(1, [4, 5, 6]));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  return (string[0] === letter);
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll( array ) {
   let sum = 0
   // TODO: loop to add items
+  for (i of array){
+    sum += i;
+  }
   return sum;
 }
+console.log ('adding 1+2+3+4+5: ', sumAll([1, 2, 3, 4, 5]));
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+function positiveArr ( array ){
+  let newArray = [];
+  for (i of array){
+    if (i > 0){
+      newArray.push(i);
+    }
+  }
+  return newArray;
+}
 
+console.log('Testing array of 1, -2, 3, -4, 5', positiveArr([1,-2,3,-4,5]));
 
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
 //     here in a comment, write the function, and test it!
+
+//Tuck in Array
+//Create a function that takes two arrays and insert the second array in the middle of the first array.
+//The first array always has two elements
+//Use spread syntax
+//https://edabit.com/challenge/7ysTEDruHz2prcJQ9
+
+function tuckIn ( arr1, arr2 ){
+  return [arr1[0], ...arr2, arr1[1]];
+}
+
+console.log('Testing with array 1 [1,2] and array 2 [3,4,5]', tuckIn([1,2], [3,4,5]));
